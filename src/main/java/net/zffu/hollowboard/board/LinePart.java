4ca67226810e34d5251542e2cddd28c33862773d
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-01 23:50:47 +0100

    feat: added global non player reliant optimization

--- src/main/java/net/zffu/hollowboard/board/LinePart.java
+++ src/main/java/net/zffu/hollowboard/board/LinePart.java
@@ -1,7 +1,9 @@
 package net.zffu.hollowboard.board;
 
 import net.zffu.hollowboard.deps.PluginAPIAccessor;
+import net.zffu.hollowboard.utils.UpdatableValue;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 /**
  * Represents a line part
@@ -43,13 +45,27 @@ public interface LinePart {
     class SkriptVariable implements LinePart {
 
         private final String variableName;
+        private final boolean isPlayerDependant;
+        private UpdatableValue<String> globalValue; // non-player dependant optimization
 
         public SkriptVariable(String variableName) {
             this.variableName = variableName;
+            this.isPlayerDependant = variableName.contains("%player%") || variableName.contains("%player-uuid%");
+
+            if(!this.isPlayerDependant) {
+                this.globalValue = new UpdatableValue<>(() -> {
+                    return PluginAPIAccessor.getSkriptVariable(variableName).toString();
+                }, 2000);
+            }
         }
 
         @Override
         public String write(Player player) {
+
+            if(!this.isPlayerDependant) {
+                return this.globalValue.gather();
+            }
+
             return PluginAPIAccessor.getSkriptVariable(variableName.replaceAll("%player%", player.getName()).replaceAll("%player-uuid%", player.getUniqueId().toString())).toString();
         }
 
@@ -62,6 +78,7 @@ public interface LinePart {
         public String toString() {
             return "SkriptVariable{" +
                     "variableName='" + variableName + '\'' +
+                    ", isPlayerDependant=" + isPlayerDependant +
                     '}';
         }
     }
