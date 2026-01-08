6c05d7597a8b040c36a709b5041f2255189c9508
Zffu <103074097+Zffu@users.noreply.github.com>
2026-01-08 22:17:50 +0100

    feat: added BoardContentLike

---
+++ src/main/java/net/zffu/hollowboard/board/components/BoardContentLike.java
@@ -0,0 +1,19 @@
+package net.zffu.hollowboard.board.components;
+
+import org.bukkit.entity.Player;
+
+import java.util.List;
+
+/**
+ * Represents a content container for a board.
+ */
+public interface BoardContentLike {
+
+    /**
+     * Writes the board content for a given user.
+     * @param player the player
+     * @return the written strings
+     */
+    List<String> write(Player player);
+
+}
